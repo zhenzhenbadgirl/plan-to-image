8468f2d0b505125c6a64829e684ec3b456dbee68
TOUGHWO\shizhenzhen <shizhenzhen@toughwo.com>
2017-04-04 00:51:06 +0800

    动作个数、秒数、描述、注意

--- index.js
+++ index.js
@@ -1,6 +1,73 @@
-var man = "liqi";
-function shizhenzhen(a, b) {
-    a = b + 5;
-    console.log(a + b);
-}
+var table = new Vue({
+    el: ".wrapper",
+    data: {
+        actionData: [
+            { Name: "翘臀分腿蹲", Intensity: 12, Circulation: 3, Unit: "个" },
+            { Name: "击掌俯卧撑", Intensity: 15, Circulation: 3, Unit: "个" },
+            { Name: "弹力带俯身内旋侧平举", Intensity: 10, Circulation: 4, Unit: "个" },
+        ],
+        describeRows: 1,
+        cautionRows: 1
+    },
+    methods: {
+        addAction: function () {
+            var newAction = {
+                Name: "",
+                Intensity: 12,
+                Circulation: 3,
+                Unit: "个"
+            };
+            this.actionData.push(newAction);
+        },
+        addDescribeRows: function () {
+            $(".plan-describe").show();
+            if (this.describeRows > 0) {
+                this.describeRows++;
+            }
+            else {
+                if (this.describeRows == 0) {
+                    this.describeRows = 1;
+                }
+            }
+        },
+        cutDescribeRows: function () {
+            if (this.describeRows > 1) {
+                this.describeRows--;
+            }
+            else {
+                if (this.describeRows == 1) {
+                    this.describeRows--;
+                    $(".plan-describe").hide();
+                }
+            }
+        },
+        addCautionRows: function () {
+            $(".table-foot").show();
+            if (this.cautionRows > 0) {
+                this.cautionRows++;
+            }
+            else {
+                if (this.cautionRows == 0) {
+                    this.cautionRows = 1;
+                }
+            }
+        },
+        cutCautionRows: function () {
+            if (this.cautionRows > 1) {
+                this.cautionRows--;
+            }
+            else {
+                if (this.cautionRows == 1) {
+                    this.cautionRows--;
+                    $(".table-foot").hide();
+                }
+            }
+        },
+        showWatermake: function () {
+            $(".watermark").toggle();
+        },
+        exportImgToPng: function () {
+        }
+    }
+});
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,72 @@
-let man = "liqi";
-function shizhenzhen(a: any, b: any) {
-    a = b + 5;
-    console.log(a + b);
-}
+var table = new Vue({
+    el: ".wrapper",
+    data: {
+        actionData: [
+            { Name: "翘臀分腿蹲", Intensity: 12, Circulation: 3, Unit: "个" },
+            { Name: "击掌俯卧撑", Intensity: 15, Circulation: 3, Unit: "个" },
+            { Name: "弹力带俯身内旋侧平举", Intensity: 10, Circulation: 4, Unit: "个" },
+        ],
+        describeRows: 1,
+        cautionRows: 1,
+    },
+    methods: {
+        addAction: function () {
+            let newAction = {
+                Name: "",
+                Intensity: 12,
+                Circulation: 3,
+                Unit: "个"
+            };
+            this.actionData.push(newAction);
+
+            // swal("hi","","info"); //为什么?
+        },
+        addDescribeRows: function () {
+            $(".plan-describe").show();
+            if (this.describeRows > 0) {
+                this.describeRows++;
+            } else {
+                if (this.describeRows == 0) {
+                    this.describeRows = 1;
+                }
+            }
+        },
+        cutDescribeRows: function () {
+            if (this.describeRows > 1) {
+                this.describeRows--;
+            } else {
+                if (this.describeRows == 1) {
+                    this.describeRows--;
+                    $(".plan-describe").hide();
+                }
+            }
+        },
+        addCautionRows: function () {
+            $(".table-foot").show();
+            if (this.cautionRows > 0) {
+                this.cautionRows++;
+            } else {
+                if (this.cautionRows == 0) {
+                    this.cautionRows = 1;
+                }
+            }
+        },
+        cutCautionRows: function () {
+            if (this.cautionRows > 1) {
+                this.cautionRows--;
+            } else {
+                if (this.cautionRows == 1) {
+                    this.cautionRows--;
+                    $(".table-foot").hide();
+                }
+            }
+        },
+        showWatermake: function () {
+            $(".watermark").toggle();
+        },
+        exportImgToPng: function () {
+            let type = "png";
+            let imgData = canvas.toDataURL(type);
+        },
+    }
+})
